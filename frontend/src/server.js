d7059a9b17b27bd860ff683713126c05c110dd31
Zaidkhan  Pathan <zaidsmac@192.168.1.9>
2024-05-13 22:51:33 +0530

    change

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,5 @@
 export const server = "/api/v2";
 
-export const backend_url = "http://localhost:8000/";
+// export const backend_url = "http://localhost:8000/";
+
+export const backend_url = "https://zaid-backend.vercel.app";

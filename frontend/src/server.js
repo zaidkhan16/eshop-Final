8faa2668850023a95dcbc6015417a5755c41230a
Zaidkhan Pathan <pathanzaidkhan99@gmail.com>
2026-08-10 13:53:47 +0530

    Update production build with relative API routes

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = process.env.REACT_APP_SERVER_URL || "http://localhost:8000/api/v2";
+export const server = process.env.REACT_APP_SERVER_URL || (process.env.NODE_ENV === "production" ? "/api/v2" : "http://localhost:8000/api/v2");
 
-export const backend_url = process.env.REACT_APP_BACKEND_URL || "http://localhost:8000/";
+export const backend_url = process.env.REACT_APP_BACKEND_URL || (process.env.NODE_ENV === "production" ? "/" : "http://localhost:8000/");
